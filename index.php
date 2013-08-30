df1b7852d5c4dc666e908be836d79f91949bd94c
Bruno Skvorc <bruno@skvorc.me>
2013-08-30 02:22:34 +0200

    Added icons, txt files and basic html framing

--- index.php
+++ index.php
@@ -6,12 +6,6 @@
     <title></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/main.css">
-    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
 </head>
 <body>
 
